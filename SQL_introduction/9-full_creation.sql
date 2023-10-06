ced1586eedaefe2db19f19eb0b9b07f255a24cd0
Amychy <119742258+Amychy@users.noreply.github.com>
2023-10-06 13:42:55 +0100

    Update 9-full_creation.sql

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,11 +1,15 @@
 -- craeting the second table
 USE hbtn_test_db_9;
 
-CREATE TABLE second_table(
+-- Create the second_table if it doesn't exist
+CREATE TABLE IF NOT EXISTS `second_table` (
+  `id` INT,
+  `name` VARCHAR(256),
+  `score` INT
+);
 
-
-CREATE TABLE IF NOT EXISTS `second_table` (`id` INT, `name` VARCHAR(256), `score` INT);
-INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, "John", 10);
-INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2, "Alex", 3);
-INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (3, "Bob", 14);
-INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (4, "George", 8);
+-- Insert data into the second_table
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, 'John', 10);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2, 'Alex', 3);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (3, 'Bob', 14);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (4, 'George', 8);
